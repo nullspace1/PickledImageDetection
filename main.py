079e7f351b99e96fa2eea71ba87ccfdffef74d0d
Lautaro Moyano <36833412+nullspace1@users.noreply.github.com>
2025-05-03 00:34:53 -0300

    Fixed mistake in loss layer

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ for epoch in range(10):
     running_loss = 0.0
     print(f"Epoch {epoch}")
     for (image,crop), (box_gt, found_gt) in loader:
-        print(image.shape)
         optimizer.zero_grad()
         box, found = model(image, crop)
         loss = loss_func.forward(box, found, box_gt, found_gt)
--- model/Loss.py
+++ model/Loss.py
@@ -10,16 +10,17 @@ class Loss(nn.Module):
 
     def forward(self,box : Tensor, found : Tensor, box_gt : Tensor, found_gt : Tensor) -> Tensor:
         box_loss = F.smooth_l1_loss(box, box_gt, reduction='sum')
-        found_loss = F.binary_cross_entropy(found, found_gt)
+        
+        found_loss = F.binary_cross_entropy(found, found_gt.reshape(-1,1), reduction='sum')
         return self.weight_box * box_loss + self.weight_found * found_loss    
     
     def test(self):
         print("Testing loss...")
-        input_box = torch.randn(1,4)
-        input_found = torch.sigmoid(torch.randn(1,1))
-        input_box_gt = torch.randn(1,4)
-        input_found_gt = torch.sigmoid(torch.randn(1,1))
+        input_box = torch.randn(2,4)
+        input_found = torch.sigmoid(torch.randn(2,1))
+        input_box_gt = torch.randn(2,4)
+        input_found_gt = torch.sigmoid(torch.randn(2,1))
         output = self.forward(input_box, input_found, input_box_gt, input_found_gt)
         print('output.shape: ', output.shape)
-        print('expected: number')
+        print('expected: ()')
         assert output.item() > 0
