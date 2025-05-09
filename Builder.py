9af98ffc80674b62c241bc32940f0f83763af077
fmaver <fmaver@frba.utn.edu.ar>
2025-05-09 20:18:13 -0300

    feat: add type hints and docstrings to DataLoader and DataBuilder classes

--- Builder.py
+++ Builder.py
@@ -6,6 +6,9 @@ import numpy as np
 class DataBuilder:
     
     def get_distortions(self, image):
+        """
+        Returns 20 distorted versions of the image with random scaling between 80% and 120% of the original size
+        """
         distortions = []
         for _ in range(20):
             x, y = random.uniform(0.8, 1.2), random.uniform(0.8, 1.2)
@@ -14,12 +17,26 @@ class DataBuilder:
         return distortions
 
     def process_image_pipeline(self, image):
+        """
+        Returns the processed image with the following steps:
+        1. Convert to grayscale
+        2. Apply Canny edge detection --> Canny edges are used to detect the edges of the image
+        3. Dilate the edges --> Dilating the edges helps to remove noise and improve the quality of the edges
+        """
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(image, 100, 255)
         edges = cv2.dilate(edges, np.ones((5,5), np.uint8), iterations=1)
         return edges
         
     def process_image(self, image_path : str):
+        """
+        Returns the processed image with the following steps:
+        1. Read the image
+        2. Process the image
+        3. Find the contours
+        4. For the contours, get the bounding boxes --> Bounding boxes are used to detect the objects in the image
+        5. Return the cropped images --> Cropped images are extracted from the bounding boxes
+        """
         image = cv2.imread(image_path)
         processed_image = self.process_image_pipeline(image)
                 
@@ -39,6 +56,14 @@ class DataBuilder:
         return cropped_images
 
     def build_data(self, screenshots_path : str, output_path : str) -> None:
+        """
+        Returns the processed image with the following steps:
+        1. Read the image
+        2. Process the image --> cropping the image to get significant objects
+        3. For each cropped image, get distortions --> 20 disrtioned images with random scaling
+        4. We save in a list a tuple of the cropped object image (that is present in the distorted image) and the distorted image
+        5. Save in a path as a numpy array the data list
+        """
         data = []
         for file in os.listdir(screenshots_path):
             if file.endswith(".png"):
--- Loader.py
+++ Loader.py
@@ -1,13 +1,21 @@
 import torch
+import numpy as np
 
 class DataLoader(torch.utils.data.Dataset):
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray):
         self.data = data
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> tuple:
+        """
+        1. Read the image and the label
+        2. Convert the image and the label to grayscale, by choosing the first channel -> all rows, all columns, first channel (grayscale)
+        3. Add a channel dimension -> all rows, all columns, all channels (RGB)
+        4. Convert the image and the label to torch tensors
+        5. Return the image and the label
+        """
         image, label = self.data[index]
         image = image[:,:,0]
         label = label[:,:,0]
