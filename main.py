fe5308ca29d30041d76347cfc5cc2e5561fb2ad6
Lautaro Moyano <36833412+nullspace1@users.noreply.github.com>
2025-05-20 16:34:28 -0300

    Added separation of sample for training and sample for validation

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Training parameters')
 parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
-parser.add_argument('--samples', type=int, default=1000, help='Number of samples to generate')
+parser.add_argument('--samples_train', type=int, default=1000, help='Number of samples to generate')
+parser.add_argument('--samples_validation', type=int, default=100, help='Number of samples to generate')
 parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
 parser.add_argument('--model_path', type=str, default="data/model.pth", help='Path to the model')
 parser.add_argument('--cache_size', type=int, default=1000, help='Cache size for the data loader')
@@ -26,9 +27,10 @@ image_processor = ImageProcessor()
 template_processor = TemplateProcessor(1000)
 hypernetwork = HyperNetwork(image_processor, template_processor)
 
-data_creator = DataCreator(args.screenshots_path, args.templates_path, args.generated_data_path, templates_per_screenshot=10, samples=args.samples)
-training_data_loader = DataLoader(args.training_data_path, data_creator, batch_size=args.batch_size)
-validation_data_loader = DataLoader(args.validation_data_path, data_creator, batch_size=args.batch_size)
+data_creator_train = DataCreator(args.screenshots_path, args.templates_path, args.generated_data_path, templates_per_screenshot=10, samples=args.samples)
+data_creator_validation = DataCreator(args.screenshots_path, args.templates_path, args.generated_data_path, templates_per_screenshot=10, samples=args.samples)
+training_data_loader = DataLoader(args.training_data_path, data_creator_train, batch_size=args.batch_size)
+validation_data_loader = DataLoader(args.validation_data_path, data_creator_validation, batch_size=args.batch_size)
 
 trainer = Trainer(hypernetwork, training_data_loader, torch.optim.Adam(hypernetwork.parameters(), lr=0.001), args.model_path)
 
